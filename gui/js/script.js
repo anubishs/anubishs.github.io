6bb8d7e0d1b0ade8329465dd14233375a594654b
anubisprimo <anubis@primo.com.br>
2025-07-25 16:36:51 -0300

    no message

--- gui/js/script.js
+++ gui/js/script.js
@@ -1,22 +1,37 @@
+// const swiper = new Swiper('.swiper-container', {
+//   loop: true,
+//   spaceBetween: 0,
+//   slidesPerView: 3,
+//   centeredSlides: false,
+//   autoplay: {
+//     delay: 10000,
+//     disableOnInteraction: false,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1024: {
+//       slidesPerView: 3,
+//     }
+//   }
+// });
 const swiper = new Swiper('.swiper-container', {
-  loop: true,
-  spaceBetween: 20,
-  slidesPerView: 1,
-  centeredSlides: true,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
+  effect: "cards",
+  grabCursor: true,
+  // loop: true,
+  cardsEffect: {
+    slideShadows: false,
+    perSlideOffset: 15, // Space between cards in px
+    // perSlideRotate: 1, // Rotation of cards in degrees
   },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    }
-  }
 });
+swiper.slideTo(2, 0)
